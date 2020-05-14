0ec5cc2d4dc81ad5e4602c6d77d98b06227db0d2
unknown <er.rajbatth@gmail.com>
2020-05-14 17:30:00 -0400

    Updates

---
+++ precache-manifest.c25f0b866a24c3ff31fb3e708ae7e7c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec007adc0f009ed1073374f47fed6fd9",
+    "url": "/React-ToDo/index.html"
+  },
+  {
+    "revision": "13c6abebd535cecd1179",
+    "url": "/React-ToDo/static/css/main.99edffd7.chunk.css"
+  },
+  {
+    "revision": "44bde1417e85594f81ab",
+    "url": "/React-ToDo/static/js/2.d6f7ac0b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-ToDo/static/js/2.d6f7ac0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13c6abebd535cecd1179",
+    "url": "/React-ToDo/static/js/main.385460fd.chunk.js"
+  },
+  {
+    "revision": "975996c7a47816a816ee",
+    "url": "/React-ToDo/static/js/runtime-main.ebfe6afc.js"
+  }
+]);
